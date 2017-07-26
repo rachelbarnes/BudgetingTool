ee027d3ef92f9421e1a03a9580d2ca45d49ae4f3
rachelbarnes <rachelbarnesjn116@yahoo.com>
2017-07-25 20:20:28 -0400

    Store Type Queries and Some Tests

---
+++ BudgetTool/Queries/StoreTypeQueries.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BudgetTool.Queries {
+    public class StoreTypeQueries {
+        public List<StoreType> ReturnAllStoreTypes() {
+            var context = new MyBudgetEntities();
+            var myStoreTypes = context.StoreType;
+            var allMyStoreTypes = (from type in myStoreTypes select type).ToList();
+            return allMyStoreTypes;
+        }
+        public List<StoreType> ReturnSelectStoreTypes(List<string> selection) {
+            var context = new MyBudgetEntities();
+            var myStoreTypes = context.StoreType;
+            var allMyStoreTypes = (from type in myStoreTypes select type).ToList();
+            var allMyStoreTypesNames = allMyStoreTypes.Select(x => x.StoreTypeName).ToList();
+            var typesToReturn = new List<StoreType>();
+            foreach (var type in selection) {
+                if (allMyStoreTypesNames.Contains(type))
+                    typesToReturn.Add(allMyStoreTypes.Where(x => x.StoreTypeName == type).First());
+            }
+            return typesToReturn;
+        }
+        public StoreType ReturnSingleStoreType(string typeToReturn) {
+            var context = new MyBudgetEntities();
+            var myStoreTypes = context.StoreType;
+            var storeTypeToReturn = (from type in myStoreTypes where type.StoreTypeName == typeToReturn select type).First();
+            return storeTypeToReturn;
+        }
+        public void AddSingleStoreType(string typeToAdd) {
+            var context = new MyBudgetEntities();
+            var myStoreTypes = context.StoreType;
+            var newStoreType = new StoreType { StoreTypeName = typeToAdd };
+            myStoreTypes.Add(newStoreType);
+            context.SaveChanges();
+        }
+        public void RemoveSingleStoreType(string typeToRemove) {
+            var context = new MyBudgetEntities();
+            var myStoreTypes = context.StoreType;
+            var storeTypeToRemove = (from type in myStoreTypes where type.StoreTypeName == typeToRemove select type).First(); 
+            myStoreTypes.Remove(storeTypeToRemove);
+            context.SaveChanges();
+        }
+        public void RemoveAllStoreTypesAvailable() {
+            var context = new MyBudgetEntities();
+            var myStoreTypes = context.StoreType;
+            var mySpendatures = context.Spendature; 
+            var allMyStoreTypes = (from type in myStoreTypes select type).ToList();
+            var allMySpendatures = (from spend in mySpendatures select spend).ToList();
+            var storeTypesInSpendatures = new List<int>(); 
+            foreach (var spend in allMySpendatures) {
+                if (!storeTypesInSpendatures.Contains(spend.StoreTypeId))
+                    storeTypesInSpendatures.Add(spend.StoreTypeId); 
+            }
+            foreach (var type in allMyStoreTypes) {
+                if (!storeTypesInSpendatures.Contains(type.StoreTypeId)){
+                    myStoreTypes.Remove(type);
+                    context.SaveChanges(); 
+                }
+            }
+            var allMyStoreTypesUpdated = (from type in allMyStoreTypes select type).ToList(); 
+        }
+        public void EditSingleStoreType(string oldType, string newType) {
+            var context = new MyBudgetEntities();
+            var myStoreTypes = context.StoreType;
+            var storeTypeToEdit = (from type in myStoreTypes where type.StoreTypeName == oldType select type).First();
+            storeTypeToEdit.StoreTypeName = newType;
+            context.SaveChanges(); 
+        }
+    }
+}
---
+++ BudgetToolTests/StoreTypeTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BudgetTool.Queries;
+using BudgetTool; 
+using NUnit.Framework; 
+namespace BudgetToolTests {
+    public class StoreTypeTests {
+        [Test]
+        public void TestStoreTypeReturnAll() {
+            var st = new StoreTypeQueries();
+            var expectedReturn = st.ReturnAllStoreTypes();
+            var context = new MyBudgetEntities();
+            var myStoreTypes = context.StoreType;
+            var actualReturn = (from type in myStoreTypes select type).ToList();
+            Assert.AreEqual(expectedReturn.Count(), actualReturn.Count()); 
+        }
+        [Test]
+        public void TestReturnSingleStoreType() {
+            var st = new StoreTypeQueries();
+            var expectedReturn = "Abby";
+            var actualReturn = st.ReturnSingleStoreType("Abby");
+            Assert.AreEqual(expectedReturn, actualReturn.StoreTypeName); 
+        }
+        [Test]
+        public void TestRemoveAllStoreTypesAvailable() {
+            var st = new StoreTypeQueries();
+            var expectedReturnCount = 2;
+            st.RemoveAllStoreTypesAvailable(); 
+            var context = new MyBudgetEntities();
+            var myStoreTypes = context.StoreType;
+            var actualReturnCount = (from type in myStoreTypes select type).ToList().Count();
+            Assert.AreEqual(expectedReturnCount, actualReturnCount); 
+        }
+        [Test]
+        public void TestEditSingleStoreType() {
+            var st = new StoreTypeQueries();
+            st.EditSingleStoreType("Food Shopping", "Groceries"); 
+            var expectedStoreTypeName = "Groceries"; 
+            var context = new MyBudgetEntities();
+            var myStoreTypes = context.StoreType;
+            var actualStoreTypeName = (from type in myStoreTypes where type.StoreTypeName == "Groceries" select type.StoreTypeName).First();
+            Assert.AreEqual(expectedStoreTypeName, actualStoreTypeName); 
+        }
+    }
+}
